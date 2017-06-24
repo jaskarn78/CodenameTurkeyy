61ffd1ef052e595b59ec7707e93e3c6c0a404ef0
jaskarnjagpal <jagpal@csus.edu>
2017-06-24 14:55:14 -0700

    Initial activitys created

--- app/src/main/java/com/example/android/hackathon/UserType.java
+++ app/src/main/java/com/example/android/hackathon/UserType.java
@@ -1,14 +1,115 @@
 package com.example.android.hackathon;
 
+import android.content.Intent;
+import android.support.percent.PercentLayoutHelper;
+import android.support.percent.PercentRelativeLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.example.android.hackathon.R;
+import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.common.SignInButton;
 
 public class UserType extends AppCompatActivity {
+    LoginButton mLoginButton;
+    private Button userLoginBtn;
+    private Button driverLoginBtn;
+    private TextView tvSignupInvoker;
+    private LinearLayout llSignup;
+    private TextView tvSigninInvoker;
+    private LinearLayout llSignin;
+    private SignInButton signInButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);        FacebookSdk.sdkInitialize(this);
         setContentView(R.layout.activity_user_type);
+
+        userLoginBtn=(Button)findViewById(R.id.userLogin);
+        driverLoginBtn=(Button)findViewById(R.id.driverLogin);
+        driverLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                driverLogin();
+            }
+        });
+
+        llSignup = (LinearLayout)findViewById(R.id.llSignup);
+        llSignin = (LinearLayout)findViewById(R.id.llSignin);
+
+        tvSignupInvoker = (TextView) findViewById(R.id.tvSignupInvoker);
+        tvSigninInvoker = (TextView) findViewById(R.id.tvSigninInvoker);
+
+        tvSignupInvoker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSignupForm();
+            }
+        });
+
+        tvSigninInvoker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSigninForm();
+            }
+        });
+        showSigninForm();
+
+    }
+
+    private void driverLogin(){
+        Intent intent = new Intent(this, DriverActivity.class);
+        startActivity(intent);
+    }
+
+
+    private void showSignupForm() {
+        PercentRelativeLayout.LayoutParams paramsLogin = (PercentRelativeLayout.LayoutParams) llSignin.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoLogin = paramsLogin.getPercentLayoutInfo();
+        infoLogin.widthPercent = 0.15f;
+        llSignin.requestLayout();
+
+
+        PercentRelativeLayout.LayoutParams paramsSignup = (PercentRelativeLayout.LayoutParams) llSignup.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoSignup = paramsSignup.getPercentLayoutInfo();
+        infoSignup.widthPercent = 0.85f;
+        llSignup.requestLayout();
+
+        tvSignupInvoker.setVisibility(View.GONE);
+        tvSigninInvoker.setVisibility(View.VISIBLE);
+        Animation translate= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.translate_right_to_left);
+        llSignup.startAnimation(translate);
+
+    }
+    private void showSigninForm() {
+        PercentRelativeLayout.LayoutParams paramsLogin = (PercentRelativeLayout.LayoutParams) llSignin.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoLogin = paramsLogin.getPercentLayoutInfo();
+        infoLogin.widthPercent = 0.85f;
+        llSignin.requestLayout();
+
+
+        PercentRelativeLayout.LayoutParams paramsSignup = (PercentRelativeLayout.LayoutParams) llSignup.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoSignup = paramsSignup.getPercentLayoutInfo();
+        infoSignup.widthPercent = 0.15f;
+        llSignup.requestLayout();
+
+        Animation translate= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.translate_left_to_right);
+        llSignin.startAnimation(translate);
+
+        tvSignupInvoker.setVisibility(View.VISIBLE);
+        tvSigninInvoker.setVisibility(View.GONE);
     }
 }
